d60be8332c31959b4a05c0daaa6cfe9109634401
ecourse <smkang@codenuri.co.kr>
2022-11-14 10:43:22 +0900

    Add files via upload

--- DAY1/2_upcasting3-1.cpp
+++ DAY1/2_upcasting3-1.cpp
@@ -1,4 +1,5 @@
-// upcasting3.cpp
+#include <iostream>
+
 class Animal
 {
 public:
@@ -19,7 +20,12 @@ void NewYear(Animal* p)
 
 	// 세월이 지나서 새로운 요구사항이 생겼다.
 	// p가 Dog 였다면 색상도 변경하고 싶다.
-	p->color = 10;
+	// p->color = 10;	// error
+
+	// Dog 고유 멤버에 접근하려면 캐스팅 해야 한다.
+	Dog* pDog = static_cast<Dog*>(p);
+
+	std::cout << pDog << std::endl;
 }
 
 int main()
