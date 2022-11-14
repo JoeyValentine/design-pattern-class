7e6bec20e358ef4dc37e582da4f99034103a05e2
ecourse <smkang@codenuri.co.kr>
2022-11-14 10:57:28 +0900

    Add files via upload

--- DAY1/2_upcasting3-2.cpp
+++ DAY1/2_upcasting3-2.cpp
@@ -17,6 +17,17 @@ void NewYear(Animal* p)
 {
 	++(p->age);
 }
+// 기존 코드의 수정 보다는
+// 새로운 코드의 추가가 좀더 안전합니다.
+
+void NewYear(Dog* p)
+{
+	// 기존의 기능을 먼저 실행하고
+	NewYear( static_cast<Animal*>(p) );
+
+	// 새로운 요구 조건을 실행
+	p->color = 10;
+}
 
 int main()
 {
