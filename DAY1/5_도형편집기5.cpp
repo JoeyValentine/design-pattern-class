86651a05f395531e4dc9aa81ca6f4910381046e6
ecourse <smkang@codenuri.co.kr>
2022-11-14 14:33:43 +0900

    Add files via upload

---
+++ DAY1/5_도형편집기5.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <vector>
+
+class Shape
+{
+	int color;
+public:
+	virtual ~Shape() {}
+
+	void set_color(int c) { color = c; }
+	int  get_color() { return c; }
+
+	virtual int get_area() { return -1; }
+
+
+	virtual void draw() { std::cout << "draw shape" << std::endl; }
+};
+
+
+class Rect : public Shape
+{
+public:
+	virtual void draw() override { std::cout << "draw rect" << std::endl; }
+};
+
+class Circle : public Shape
+{
+public:
+	virtual void draw() override { std::cout << "draw circle" << std::endl; }
+};
+
+class Triangle : public Shape
+{
+public:
+	virtual void draw() override { std::cout << "draw triangle" << std::endl; }
+};
+
+int main()
+{
+	std::vector<Shape*> v;
+
+	while (true)
+	{
+		int cmd;
+		std::cin >> cmd;
+
+		if (cmd == 1)
+		{
+			v.push_back(new Rect);
+		}
+		else if (cmd == 2)
+		{
+			v.push_back(new Circle);
+		}
+		else if (cmd == 9)
+		{
+			for (auto p : v)
+				p->draw();	
+		}
+
+		else if (cmd == 8)
+		{
+			int k = -1;
+			std::cout << "몇번째를 복사할까요 >> ";
+			std::cin >> k;
+
+			// k번째 만들었던 도형의 복사본을 생성해서 v끝에 추가합니다.
+			// 그런데, k번째 도형은 어떤 도형일까요 ?
+			// 어떻게 코드를 작성해야 할까요 ?
+		}
+	}
+}
+
