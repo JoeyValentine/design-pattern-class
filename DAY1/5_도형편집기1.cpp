a56ab723c2351e1a443fd0aaeb6964e530798b59
ecourse <smkang@codenuri.co.kr>
2022-11-14 13:49:14 +0900

    Add files via upload

--- DAY1/5_도형편집기1.cpp
+++ DAY1/5_도형편집기1.cpp
@@ -3,14 +3,22 @@
 
 // 1. 모든 도형을 타입으로 설계하면 편리하다.
 
+// 2. 모든 도형의 기반클래스(Shape) 가 있다면 
+//    모든 도형을 한개의 컨테이너(컬렉션)에 보관할수 있다.
 
-class Rect
+class Shape
+{
+public:
+	virtual ~Shape() {}
+};
+
+class Rect : public Shape
 {
 public:
 	void draw() { std::cout << "draw rect" << std::endl; }
 };
 
-class Circle
+class Circle : public Shape
 {
 public:
 	void draw() { std::cout << "draw circle" << std::endl; }
@@ -18,6 +26,7 @@ public:
 
 int main()
 {
-	std::vector<Rect*> v; // ?
+//	std::vector<Rect*>	v; // Rect 만 보관
+	std::vector<Sahpe*> v; // 모든 도형을 보관!!!
 }
 
