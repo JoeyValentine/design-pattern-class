3741f9f23305a5e4aa6cbdabe02931df8854f606
ecourse <smkang@codenuri.co.kr>
2022-11-14 09:33:12 +0900

    Add files via upload

--- DAY1/1_생성자1-1.cpp
+++ DAY1/1_생성자1-1.cpp
@@ -11,12 +11,20 @@ public:
 class Derived : public Base
 {
 public:
-	Derived() {	}
-	Derived(int a){}
+	// 핵심 1 : 기반 클래스에 디폴트 생성자가 없으면 아래 2줄은 에러입니다.
+//	Derived()     {}  // Derived()      : Base() {}
+//	Derived(int a){}  // Derived(int a) : Base(){}
+
+	// 핵심 2 : 기반 클래스에 디폴트 생성자가 없다면
+	// => 반드시 파생 클래스를 만들때 기반 클래스의 생성자를 명시적으로
+	//    호출해야 합니다.
+	Derived()     : Base(0) {}  
+	Derived(int a): Base(a) {}
 };
 
 int main()
 {
-
+	Derived d1;
+	Derived d2(5);
 }
 
