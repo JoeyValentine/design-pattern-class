381b92d328e8122d29ee1f47261f979ecee8134d
ecourse <smkang@codenuri.co.kr>
2022-11-15 11:06:21 +0900

    Add files via upload

--- DAY2/1_메뉴4.cpp
+++ DAY2/1_메뉴4.cpp
@@ -63,12 +63,22 @@ public:
 
 int main()
 {
-	MenuItem m1("±è¹ä", 11);
-	MenuItem m2("¶ó¸é", 12);
+	MenuItem m1("ÂüÄ¡ ±è¹ä",   11);
+	MenuItem m2("¼Ò°í±â ±è¹ä", 12);
+	MenuItem m3("µ·±î½º ±è¹ä", 13);
+
+	MenuItem m4("¶ó¸é", 20);
+
+	PopupMenu kimbam_pm("±è¹ä·ù");
+	kimbam_pm.add_menu(&m1);
+	kimbam_pm.add_menu(&m2);
+	kimbam_pm.add_menu(&m3);
+
 
 	PopupMenu pm("¿À´ÃÀÇ ¸Þ´º");
-	pm.add_menu(&m1);
-	pm.add_menu(&m2);
+	pm.add_menu(&kimbam_pm); // ÇÙ½É!!!!
+							// popup Àº ´Ù½Ã popupÀ» º¸°üÇØ¾ß ÇÕ´Ï´Ù.
+	pm.add_menu(&m4);
 
 	pm.command();  
 
