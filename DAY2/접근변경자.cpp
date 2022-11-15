1673622380ca38d08936c98458bff1107591d707
ecourse <smkang@codenuri.co.kr>
2022-11-15 17:05:37 +0900

    Add files via upload

--- DAY2/접근변경자.cpp
+++ DAY2/접근변경자.cpp
@@ -6,3 +6,25 @@
 // SWIFT : class Derived : Base {}
 // Python: class Derived(Base) {}
 // Java  : class Derived extends Base {}
+
+
+class Base
+{
+private:   int a;
+protected: int b;
+public:    int c;
+};
+
+// 접근 변경자 : 기반 클래스가 가진 멤버의 보호속성을 "축소할때" 사용
+class Derived : public Base
+{
+};
+
+int main()
+{
+	Base    base;
+	Derived derv;
+
+	base.c = 10; // ok
+	derv.c = 10; // ?
+}
