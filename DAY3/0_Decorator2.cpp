e7db5b1277e363cf5f23b52b60c6143f51776148
ecourse <smkang@codenuri.co.kr>
2022-11-16 08:46:32 +0900

    Add files via upload

--- DAY3/0_Decorator2.cpp
+++ DAY3/0_Decorator2.cpp
@@ -15,11 +15,10 @@ class LeftMissile
 public:
 	LeftMissile(SpaceShip* s) : ship(s) {}
 
-
 	void Fire()
 	{
-		SpaceShip::Fire();
-		std::cout << "Fire Left Missile" << std::endl;
+		ship->Fire();	// 기존 객체의 기능을 사용하고
+		std::cout << "Fire Left Missile" << std::endl; // 새로운 기능을 추가
 	}
 };
 
@@ -28,6 +27,7 @@ int main()
 	SpaceShip ss;
 	ss.Fire();
 
-	LeftMissile lm; 
+	LeftMissile lm(&ss);	// ss 라는 이미 존재 하는 객체에 
+							// 새로운 기능을 추가하는 LeftMissile 객체
 	lm.Fire();
 }
