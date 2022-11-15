80d67719d4534a3c1bf0799910f5aee480da6479
ecourse <smkang@codenuri.co.kr>
2022-11-15 17:02:13 +0900

    Add files via upload

---
+++ DAY2/접근변경자.cpp
@@ -0,0 +1,8 @@
+// 접근변경자
+
+// 상속을 표기 법
+// C++   : class Derived : public Base {}
+// C#    : class Derived : Base {}
+// SWIFT : class Derived : Base {}
+// Python: class Derived(Base) {}
+// Java  : class Derived extends Base {}
