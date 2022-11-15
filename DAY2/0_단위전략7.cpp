5b0ce417e2b779081cf479af72587cb8a132fe54
ecourse <smkang@codenuri.co.kr>
2022-11-15 10:01:10 +0900

    Add files via upload

--- DAY2/0_단위전략7.cpp
+++ DAY2/0_단위전략7.cpp
@@ -4,6 +4,8 @@
 // STL 컨테이너에 전달할 메모리 할당기 만들기
 // => 반드시 지켜야 하는 규칙이 "C++표준문서" 에 정리되어 있습니다.
 // 1. allocate, deallocate 멤버 함수를 만들어야 합니다.
+// 2. 디폴트 생성자, 템플릿 생성자, value_type 멤버가 있어야 합니다.
+//    => 관례적으로 필요한 코드들..
 
 template<typename T> class debug_alloc
 {
@@ -14,12 +16,16 @@ public:
 		printf("allocate %d cnts %p\n", sz, p);
 		return static_cast<T*>(p);
 	}
-
 	void deallocate(T* p, std::size_t sz)
 	{
 		printf("deallocate %d cnts %p\n", sz, p);
 		free(p);
 	}
+
+	// 아래 3개의 멤버도 필요 합니다
+	using value_type = T;
+	debug_alloc() {}
+	template<typename U> debug_alloc(const debug_alloc<U>&) {}
 };
 
 
