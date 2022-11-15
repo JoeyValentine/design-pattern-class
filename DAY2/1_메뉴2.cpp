4333888d49a6b322b311c6678f694f3208e9ce07
ecourse <smkang@codenuri.co.kr>
2022-11-15 10:40:43 +0900

    Add files via upload

---
+++ DAY2/1_메뉴2.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <conio.h> 
+
+// 객체지향 프로그래밍 : 모든 것은 객체이다.
+//                프로그램이 기본 단위는 "함수" 가 아닌 "클래스"이다
+
+class MenuItem
+{
+	std::string title;
+	int id;
+public:
+	MenuItem(const std::string& title, int id)
+		: title(title), id(id) {}
+
+	std::string get_title() { return title; }
+
+	void command()
+	{
+		std::cout << get_title() << "메뉴 선택됨" << std::endl;
+		_getch(); // 아무키나 누를때 까지 대기
+	}
+};
+
+
+int main()
+{
+	// 각각의 메뉴 항목을 모두 객체로 취급
+	MenuItem m1("김밥", 11);
+	MenuItem m2("라면", 12);
+
+	m1.command(); // 메뉴 선택 
+}
+
+
+
+
