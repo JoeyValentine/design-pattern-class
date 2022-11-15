5db1f46ab8a384fa1ba1cfed82e237e78d695fac
ecourse <smkang@codenuri.co.kr>
2022-11-15 12:06:18 +0900

    Add files via upload

---
+++ DAY2/1_메뉴6.cpp
@@ -0,0 +1,96 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <conio.h> 
+
+
+class BaseMenu
+{
+	std::string title;
+public:
+	BaseMenu(const std::string& title) : title(title) {}
+	virtual ~BaseMenu() {}
+	std::string get_title() { return title; }
+	virtual void command() = 0;
+};
+
+class MenuItem : public BaseMenu
+{
+	int id;
+public:
+	MenuItem(const std::string& title, int id)
+		: BaseMenu(title), id(id) {}
+
+	virtual void command() override
+	{
+		std::cout << get_title() << " 메뉴 선택됨" << std::endl;
+		_getch();
+	}
+};
+
+class PopupMenu : public BaseMenu
+{
+	std::vector<BaseMenu*> v;
+public:
+	PopupMenu(const std::string& title) : BaseMenu(title) {}
+
+	void add_menu(BaseMenu* p) { v.push_back(p); }
+
+	virtual void command() override
+	{
+		while (1)
+		{
+			system("cls");
+
+			int sz = v.size();
+			for (int i = 0; i < sz; i++)
+			{
+				std::cout << i + 1 << ". " << v[i]->get_title() << std::endl;
+			}
+			std::cout << sz + 1 << ". 종료" << std::endl;
+
+			std::cout << "메뉴를 선택하세요 >> ";
+
+			int cmd;
+			std::cin >> cmd;
+
+			if (cmd == sz + 1)
+				break;
+
+			if (cmd < 1 || cmd > sz + 1)
+				continue;
+
+			v[cmd - 1]->command();
+		}
+
+	}
+
+	BaseMenu* get_submenu(int idx) { return v[idx]; }
+};
+
+int main()
+{
+	PopupMenu* root = new PopupMenu("ROOT");
+	PopupMenu* pm1 = new PopupMenu("해상도 변경");
+	PopupMenu* pm2 = new PopupMenu("색상 변경");
+
+	root->add_menu(pm1);
+	root->add_menu(pm2);
+
+	// 아래 처럼 사용하고 싶다면
+//	root->get_submenu(0)->add_menu(new MenuItem("HD", 11));
+
+	// 방법 1. get_submenu() 의 반환 타입을 PopupMenu 로 
+	//        캐스팅해서 사용하자.
+	static_cast<PopupMenu*>(root->get_submenu(0))->add_menu(new MenuItem("HD", 11));
+
+	// 방법 2. BaseMenu 에 "get_submenu" 를 제공하자!!
+	// => 다음 예제에서 구현(메뉴7)
+
+	root->command();
+
+}
+
+
+
+
