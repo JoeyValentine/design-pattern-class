cbcf7033d22dbd4d492745b5c8e08a408090156b
ecourse <smkang@codenuri.co.kr>
2022-11-15 11:18:01 +0900

    Add files via upload

--- DAY2/1_메뉴4.cpp
+++ DAY2/1_메뉴4.cpp
@@ -3,15 +3,26 @@
 #include <vector>
 #include <conio.h> 
 
-class MenuItem
-{
+
+
+// 모든 메뉴의 공통의 기반 클래스
+class BaseMenu
+{	
 	std::string title;
+public:
+	BaseMenu(const std::string& title) : title(title) {}
+
+	virtual ~BaseMenu() {}
+
+	std::string get_title() { return title; }
+};
+
+class MenuItem : public BaseMenu
+{
 	int id;
 public:
 	MenuItem(const std::string& title, int id)
-		: title(title), id(id) {}
-
-	std::string get_title() { return title; }
+		: BaseMenu(title), id(id) {}
 
 	void command()
 	{
@@ -20,15 +31,13 @@ public:
 	}
 };
 
-
-class PopupMenu
+class PopupMenu : public BaseMenu
 {
-	std::string title;
-	std::vector<MenuItem*> v;
+	std::vector<BaseMenu*> v;
 public:
-	PopupMenu(const std::string& title) : title(title) {}
+	PopupMenu(const std::string& title) : BaseMenu(title) {}
 
-	void add_menu(MenuItem* p) { v.push_back(p); }
+	void add_menu(BaseMenu* p) { v.push_back(p); }
 
 	void command()
 	{
