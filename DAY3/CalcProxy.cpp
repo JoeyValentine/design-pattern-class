36ea0a26ec6a4358cd8c61fca4e159e1c425b67b
ecourse <smkang@codenuri.co.kr>
2022-11-16 14:20:20 +0900

    Add files via upload

--- DAY3/CalcProxy.cpp
+++ DAY3/CalcProxy.cpp
@@ -16,7 +16,7 @@ public:
 // 동적모듈을 사용하는 client 는 동적 모듈내의 proxy 클래스 이름을
 // 알지도 못하고, 알필요도 없고, 알아도 안됩니다.
 // => 동적 모듈 내부에서 Proxy 객체의 생성을 제공해야 합니다.
-extern "C" __declspec(dllexprt)
+extern "C" __declspec(dllexport)
 ICalc* Create()
 {
 	return new Calc;
@@ -28,3 +28,9 @@ ICalc* Create()
 // => 소스가 있는 폴더로 이동후
 // 
 // cl CalcProxy.cpp /LD /link user32.lib gdi32.lib kernel32.lib
+
+// D:      => D 드라이브 이동
+// cd 폴더  => 폴더로 이동
+// cd ..   => 상위 폴더이동.
+
+
