1fb6a8251e5a4957c1ec4803db932c364425fee8
ecourse <smkang@codenuri.co.kr>
2022-11-16 17:09:52 +0900

    Add files via upload

--- DAY3/4_Bridge2-1.cpp
+++ DAY3/4_Bridge2-1.cpp
@@ -14,15 +14,19 @@ void PointImpl::print() { }
 //======================================
 // Bridge - 추상층
 // Point.h
+class PointImpl; // 클래스 전방선언
+				// 만 있어도
+				// 포인터 변수는 사용가능
 class Point
 {
-	Point* pImpl;
+	PointImpl* pImpl;
 public:
 	Point();
 	void print();
 };
 
 // Point.cpp
+#include "PointImpl.h"
 #include "Point.h"
 
 Point::Point() { pImpl = new PointImpl; }
@@ -42,3 +46,10 @@ int main()
 }
 
 
+// 위 기술을
+// "PIMPL" 이라고 합니다
+
+// 1. 컴파일 시간 속도가 빠라지고
+//   (구현부가 변해도, 사용자 코드는 다시 빌드될 필요 없다.)
+
+// 2. 구현부의 완벽한 은닉이 가능하다.
