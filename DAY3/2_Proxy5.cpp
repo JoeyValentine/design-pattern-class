6582cd36b96539523d7219802cce02afb24c7392
ecourse <smkang@codenuri.co.kr>
2022-11-16 14:09:42 +0900

    Add files via upload

---
+++ DAY3/2_Proxy5.cpp
@@ -0,0 +1,20 @@
+#define USING_GUI
+#include "cppmaster.h"
+#include "ICalc.h"
+
+int main()
+{
+	ICalc* pCalc = ? ;
+	
+
+	int n1 = pCalc->Add(10, 20);
+	int n2 = pCalc->Sub(10, 20);
+
+	std::cout << n1 << ", " << n2 << std::endl;
+
+}
+
+
+
+
+
