ed679c81e1ab22cd4227bedfabc2ebd9abc3d0cc
ecourse <smkang@codenuri.co.kr>
2022-11-16 10:47:21 +0900

    Add files via upload

--- DAY3/1_Adapter2.cpp
+++ DAY3/1_Adapter2.cpp
@@ -36,6 +36,7 @@ public:
 // 클래스 어답터 : 클래스의 인터페이스(함수이름) 변경
 // 객체   어답터 : 객체의 인터페이스 변경
 
+// 클래스 어답터 입니다.
 class Text : public Shape, public TextView
 {
 public:
@@ -44,6 +45,16 @@ public:
 	void draw() { TextView::Show(); };
 };
 
+// 아래 코드가 "객체 어답터" 입니다.
+class Text2 : public Shape
+{
+	TextView* txtView; // 핵심 : 포인터 또는 참조로 포함
+						// 외부에 이미 생성되어 있는 객체를 
+						// 가리키겠다는 의도!!
+public:
+	Text2(TextView* tv) : txtView(tv) {}
+	void draw() { txtView->Show(); };
+};
 int main()
 {
 	std::vector<Shape*> v;
@@ -53,7 +64,9 @@ int main()
 
 	// 이미 존재하는 객체 "tv" 를 v에 넣고 싶습니다.
 
-	v.push_back( &tv ); // error!!. Shape로 부터 파생되지 않았습니다.
+	//v.push_back( &tv ); // error!!. Shape로 부터 파생되지 않았습니다.
+
+	v.push_back( new Text2(&tv) );
 }
 
 
