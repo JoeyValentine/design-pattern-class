3d8b06b85aec6d72d00fc45290fb7693ce74fbff
ecourse <smkang@codenuri.co.kr>
2022-11-16 09:30:19 +0900

    Add files via upload

--- DAY3/0_Decorator5.cpp
+++ DAY3/0_Decorator5.cpp
@@ -20,6 +20,12 @@ public:
 
 int main()
 {
+	// C언어의 문제점 : 개발자가 직접 자원을 관리해야 한다.
 	FILE* f = fopen("a.txt", "wt");
 	fclose(f);
+
+	// 객체지향(C++) 을 사용하면
+	// => 생성자/소멸자로 자원 관리를 하면 됩니다.
+	FileStream fs("a.txt");
+	fs.Write("hello");
 }
