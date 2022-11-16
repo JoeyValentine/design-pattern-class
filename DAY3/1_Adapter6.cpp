d847944a47dc560a5f7e29e865ca1f05d0a34369
ecourse <smkang@codenuri.co.kr>
2022-11-16 11:35:36 +0900

    Add files via upload

--- DAY3/1_Adapter6.cpp
+++ DAY3/1_Adapter6.cpp
@@ -3,17 +3,22 @@
 #include <vector>
 #include <deque>
 
-template<typename T> class stack
+template<typename T, typename C = std::deque<T> > 
+class stack
 {
-	std::list<T> c; 
+	C c; 
 public:
 	void push(const T& a) { c.push_back(a); }
 	void pop()				{ c.pop_back(); }
 	T& top()				{ return c.back(); }
 };
-
 int main()
 {
-	stack<int> s;
-	s.push(10);
+	stack<int> s1; // std::deque
+	stack<int, std::list<int>> s2;
+
+	stack<int, std::vector<int>> s3; // std::vector 를
+									 // stack 처럼 사용할수 있도록
+									 // 변경해 달라는 것.
+
 }
