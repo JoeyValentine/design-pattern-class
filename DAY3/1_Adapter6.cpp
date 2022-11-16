3a4438100661c1d6df447ce7617f547e87ba44ba
ecourse <smkang@codenuri.co.kr>
2022-11-16 11:40:42 +0900

    Add files via upload

--- DAY3/1_Adapter6.cpp
+++ DAY3/1_Adapter6.cpp
@@ -2,23 +2,30 @@
 #include <list>
 #include <vector>
 #include <deque>
-
+/*
 template<typename T, typename C = std::deque<T> > 
 class stack
 {
 	C c; 
 public:
-	void push(const T& a) { c.push_back(a); }
-	void pop()				{ c.pop_back(); }
-	T& top()				{ return c.back(); }
+	inline void push(const T& a) { c.push_back(a); }
+	inline void pop()				{ c.pop_back(); }
+	inline T& top()				{ return c.back(); }
 };
+*/
+#include <stack> // C++ 표준의 stack 이 위처럼 되어 있습니다.
+				 // 흔히 "stack adapter" 라는 용어를 사용합니다.
+				 // Policy Base Design 으로 "컨테이너 교체 가능합니다."
+					// 컨테이너에는 반드시 push_back, pop_back, back()
+					// 이 있어야 합니다.
 int main()
 {
-	stack<int> s1; // std::deque
-	stack<int, std::list<int>> s2;
+	std::stack<int> s1; // std::deque
+	std::stack<int, std::list<int>> s2;
 
-	stack<int, std::vector<int>> s3; // std::vector 를
+	std::stack<int, std::vector<int>> s3; // std::vector 를
 									 // stack 처럼 사용할수 있도록
 									 // 변경해 달라는 것.
+	s1.push(10);
 
 }
