3df31e1964f982f7ef30de81208ccf0e11d34979
ecourse <smkang@codenuri.co.kr>
2022-11-16 16:20:27 +0900

    Add files via upload

---
+++ DAY3/3_facade3.cpp
@@ -0,0 +1,85 @@
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#include <iostream>
+#include <WinSock2.h>
+#pragma comment(lib, "ws2_32.lib")
+using namespace std;
+class NetworkInit
+{
+public:
+	NetworkInit()
+	{
+		WSADATA w;
+		WSAStartup(0x202, &w);
+	}
+	~NetworkInit()
+	{
+		WSACleanup();
+	}
+};
+class IPAddress
+{
+	SOCKADDR_IN addr;
+public:
+	IPAddress(const char* ip, short port)
+	{
+		addr.sin_family = AF_INET;
+		addr.sin_port = htons(port);
+		addr.sin_addr.s_addr = inet_addr(ip);
+	}
+	SOCKADDR* getRawAddress()
+	{
+		return (SOCKADDR*)&addr;
+	}
+};
+
+class Socket
+{
+	int sock;
+public:
+	Socket(int type) { sock = socket(PF_INET, type, 0); }
+
+	void Bind(IPAddress* ip)
+	{
+		::bind(sock, ip->getRawAddress(), sizeof(SOCKADDR_IN));
+	}
+	void Listen() { ::listen(sock, 5); }
+
+	void Accept()
+	{
+		struct sockaddr_in addr2 = { 0 };
+		int sz = sizeof(addr2);
+
+		accept(sock, (SOCKADDR*)&addr2, &sz);
+	}
+};
+
+// TCPServer 를 만들려면
+// => 여러개의 클래스를 사용해서
+// => 주어진 절차를 수행해야 합니다.
+
+// 이런 과정을 간단하게 해주는 "포괄적 개념의 상위 클래스"를 제공합니다.
+// => "facade 패턴"
+
+class TCPServer
+{
+	NetworkInit init;
+	Socket sock{ SOCK_STREAM }; // TCP 서버
+public:
+	void Start(const char* ip, short port)
+	{
+		IPAddress addr(ip, port);
+		sock.Bind(&addr);
+		sock.Listen();
+		sock.Accept();
+	}
+};
+
+int main()
+{
+	// 결국 최종사용자의 코드는 아래와 같습니다.
+	TCPServer server;
+	server.Start("127.0.0.1", 4000);
+
+
+
+}
