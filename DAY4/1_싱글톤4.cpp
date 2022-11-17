ff06fb26de631e84a1d00aa4441ba020e690073d
ecourse <smkang@codenuri.co.kr>
2022-11-17 09:09:44 +0900

    Add files via upload

--- DAY4/1_싱글톤4.cpp
+++ DAY4/1_싱글톤4.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+// 방법 3. 유일한 한개의 객체를 힙에 생성하는 모델
 
 class Cursor
 {
@@ -7,14 +8,18 @@ private:
 
 	Cursor(const Cursor&) = delete;
 	Cursor operator=(const Cursor&) = delete;
+
+	static Cursor* pinstance;
 public:
 	static Cursor& getInstance()
 	{
-
-		static Cursor instance;
+		if (pinstance == nullptr)
+			pinstance = new Cursor;
 		return instance;
 	}
 };
+Cursor* Cursor::pinstance = nullptr;
+
 
 int main()
 {
