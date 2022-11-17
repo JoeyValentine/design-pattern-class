34e24857978cdb08fbf7ab666118853e48066406
ecourse <smkang@codenuri.co.kr>
2022-11-17 11:43:26 +0900

    Add files via upload

--- DAY4/3_Factory2.cpp
+++ DAY4/3_Factory2.cpp
@@ -13,12 +13,20 @@ class Rect : public Shape
 {
 public:
 	void draw() override { std::cout << "draw Rect" << std::endl; }
+
+	static Shape* Create() { return new Rect; }
 };
+// Rect 를 생성하는 방법
+// 1. Rect* p1 = new Rect;
+// 2. Shape* p2 = Rect::Create();
+
 
 class Circle : public Shape
 {
 public:
 	void draw() override { std::cout << "draw Circle" << std::endl; }
+
+	static Shape* Create() { return new Circle; }
 };
 //=======================
 // 함수 인자에 따라 다른 도형을 만들고 싶습니다.
@@ -42,15 +50,32 @@ Shape* Create(int arg)
 // 방법 2. 클래스 이름을 문자열로 받기
 Shape* Create( std::string arg)
 {
-	Shape* p = nullptr;
+	// C#, swift 등은 "Rect" 처럼 문자열로 을 가지고
+	// 객체를 생성할수 있습니다.
+	// => 아주 유용하게 활용가능하지만!!
+	// => 메모리에 정보를 보관해야 하므로 오버헤드가 큽니다.
 
-	return p;
+	// C++은 할수 없습니다.
 }
-
 int main()
 {
 	Shape* p1 = Create("Rect");
 	Shape* p2 = Create("Triangle");
+}
+
+// 방법 3. 자신의 객체를 만들수 static 멤버 함수는 항상 유용하게
+//        사용됩니다.
+Shape* Create(Shape* (*f)())
+{
+	Shape* p = f();
+	return p;
+}
+int main()
+{
+	// 인자로 클래스 이름을 보낼수 없지만
+	// 클래스의 객체를 생성하는 함수는 보낼수 있습니다.
+	Shape* p1 = Create(&Rect::Create);
+	Shape* p2 = Create(&Circle::Create);
 }
 
 
