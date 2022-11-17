bbbbcc516494ee8bd7fe982ea9a2a40a797f9cc8
ecourse <smkang@codenuri.co.kr>
2022-11-17 17:02:06 +0900

    Add files via upload

--- DAY4/6_iterator6.cpp
+++ DAY4/6_iterator6.cpp
@@ -8,12 +8,12 @@ int main()
 	std::list<int> ds = { 1,2,3,4,5 }; // 더블 링크드 리스트
 	std::forward_list<int> fs = { 1,2,3,4,5 }; // 싱글 링크드 리스트
 
-	auto p1 = ds.begin();
-	auto p2 = fs.begin();
+	auto p1 = ds.begin();  // 더블리스트의 반복자 : 양방향 반복자
+	auto p2 = fs.begin();  // 싱글리스트의 반복자 : 전진형 반복자(--안됨)
 
 	// 다음중 에러를 찾으세요
 	++p1;	
 	++p2;
 	--p1;
-	--p2;
+	--p2; // error
 }
