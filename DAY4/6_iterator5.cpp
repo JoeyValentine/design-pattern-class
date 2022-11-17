d775443bc844e0a3b8eac9d362107e959c4c49b9
ecourse <smkang@codenuri.co.kr>
2022-11-17 16:56:44 +0900

    Add files via upload

--- DAY4/6_iterator5.cpp
+++ DAY4/6_iterator5.cpp
@@ -9,11 +9,15 @@ int main()
 	std::list<int> ss = { 1,2,3,4,5 };
 
 	auto p1 = ss.begin();	// 1번째 요소를 가리키는 반복자
-	auto p2 = ss.end();		// 마지막 다음
+	auto p2 = ss.end();		// 마지막 다음(요소 순회시 끝에 도달여부 확인을 위해)
+
+	// p1 은 raw pointer 는 아닙니다.
+	// 그런데, 포인터 처럼 사용할수 있게 하기위해 "연산자 재정의" 되어 있습니다.
 
 	while (p1 != p2)
 	{
 		std::cout << *p << std::endl;
-		++p1;
+		//++p1;
+		p1.operator++(); // p1.next()
 	}
 }
