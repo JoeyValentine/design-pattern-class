4fdf32b65a1b302e43ec24e5ceef7a57f1a8b03d
ecourse <smkang@codenuri.co.kr>
2022-11-17 09:14:42 +0900

    Add files via upload

---
+++ DAY4/1_싱글톤5.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <mutex>
+
+// 힙에 만드는 모델과 멀티스레드 안정성
+
+class Cursor
+{
+private:
+	Cursor() { std::cout << "Cursor()" << std::endl; }
+
+	Cursor(const Cursor&) = delete;
+	Cursor operator=(const Cursor&) = delete;
+
+	static Cursor* pinstance;
+	static std::mutex mtx;
+public:
+
+	static Cursor& getInstance()
+	{
+		mtx.lock();
+
+		if (pinstance == nullptr)
+			pinstance = new Cursor;
+	
+		mtx.unlock();
+		return instance;
+	}
+};
+Cursor* Cursor::pinstance = nullptr;
+std::mutex Cursor::mtx;
+
+
+int main()
+{
+	Cursor& c1 = Cursor::getInstance();
+	Cursor& c2 = Cursor::getInstance();
+
+}
+
