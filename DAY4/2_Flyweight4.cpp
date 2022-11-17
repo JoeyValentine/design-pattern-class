f75304e7d6963ea2b2206d6074be8ecc3f905661
ecourse <smkang@codenuri.co.kr>
2022-11-17 11:07:46 +0900

    Add files via upload

--- DAY4/2_Flyweight4.cpp
+++ DAY4/2_Flyweight4.cpp
@@ -3,8 +3,10 @@
 int main()
 {
 	// 문자열 배열 vs 문자열 포인터
-	char sa[]      = "hello";
-	const char* sp = "hello";
+	char sa[]      = "hello"; // 문자열은 "스택"에
+	const char* sp = "hello"; // 문자열은 "상수 메모리에"
+//	char* sp = "hello"; // C언어는 이코드를 허용하지만
+//	*sp = 'A'; // 이순간 runtime eror.. 상수 메모리는 쓸수 없다.
 
 	//-----------------------
 
@@ -13,4 +15,6 @@ int main()
 
 	const char* sp1 = "hello";
 	const char* sp2 = "hello";
+
+	printf("%p, %p\n", sp1, sp2);
 }
