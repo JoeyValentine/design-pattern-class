d1fb7769d12eb90f5010e48c0b780e45b245d1e3
ecourse <smkang@codenuri.co.kr>
2022-11-17 10:08:15 +0900

    Add files via upload

--- DAY4/1_싱글톤8.cpp
+++ DAY4/1_싱글톤8.cpp
@@ -1,35 +1,51 @@
 #include <iostream>
 #include <mutex>
 
-class Cursor
+// CRTP ( Curiously Recurring Template Pattern )
+// => 기반 클래스 만들때 파생 클래스 이름을 활용할수 있게 하는 기술
+// => 파생 클래스가 자신의 이름을 기반 클래스의 템플릿 인자로 전달하는 기술
+// => 요즘 오픈소스가 정말 많이 사용하는 "C++ IDioms" 기술
+
+template<typename T> 
+class Singleton
 {
-private:
-	Cursor() { std::cout << "Cursor()" << std::endl; }
+protected:
+	Singleton() { }
 
-	Cursor(const Cursor&) = delete;
-	Cursor operator=(const Cursor&) = delete;
+	Singleton(const Singleton&) = delete;
+	Singleton operator=(const Singleton&) = delete;
 
-	static Cursor* pinstance;
+	static T* pinstance; // <<==== 1
 	static std::mutex mtx;
 public:
-	static Cursor& getInstance()
+	static T& getInstance()		// <<====== 2
 	{
 		std::lock_guard<std::mutex> g(mtx);
 
 		if (pinstance == nullptr)
-			pinstance = new Cursor;
+			pinstance = new T;   // <==== 3
 		
 		return *pinstance;
 	}
 };
-Cursor* Cursor::pinstance = nullptr;
-std::mutex Cursor::mtx;
+
+template<typename T> T* Singleton<T>::pinstance = nullptr;   // <<=== 4
+template<typename T> std::mutex Singleton<T>::mtx;
+
+
+
+
+// Mouse 클래스도 "Cursor 처럼 힙에 만드는 싱글톤 모델" 로 하고싶습니다
+class Mouse : public Singleton< Mouse  > 
+{
+
+};
 
 
 int main()
 {
-	Cursor& c1 = Cursor::getInstance();
-	Cursor& c2 = Cursor::getInstance();
+	Mouse& m1 = Mouse::getInstance();
+	Mouse& m2 = Mouse::getInstance();
 
 }
 
