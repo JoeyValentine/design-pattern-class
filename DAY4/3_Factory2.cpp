5d31c58b18657fcdd9d8487895a4f701a7096405
ecourse <smkang@codenuri.co.kr>
2022-11-17 11:35:50 +0900

    Add files via upload

--- DAY4/3_Factory2.cpp
+++ DAY4/3_Factory2.cpp
@@ -23,11 +23,34 @@ public:
 //=======================
 // 함수 인자에 따라 다른 도형을 만들고 싶습니다.
 // 단, 미래에 만들어질 도형도 가능해야 합니다.
-Shape* Create(? arg)
-{
 
+// 방법 1. 아래 처럼 하면 새로운 도형이 추가되면 코드가 수정되어야 합니다.
+/*
+Shape* Create(int arg)
+{
+	Shape* p = nullptr;
+
+	switch (arg)
+	{
+	case 1: p = new Rect; break;
+	case 2: p = new Circle; break;
+	}
+	return p;
 }
+*/
+
+// 방법 2. 클래스 이름을 문자열로 받기
+Shape* Create( std::string arg)
+{
+	Shape* p = nullptr;
+
+	return p;
+}
+
 int main()
 {
-	Shape* p = Create(? );
+	Shape* p1 = Create("Rect");
+	Shape* p2 = Create("Triangle");
 }
+
+
